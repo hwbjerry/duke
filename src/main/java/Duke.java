4d9709aa739810396bcc574499b3fb56ca5731ba
jerryhwb <hwbjerry@gmail.com>
2019-08-24 03:56:41 +0800

    Week 2 tasks completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,128 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        //level1();
+        //level2ArrayList();
+        //level2ArrayofString();
+        level3();
     }
+
+    static void level1(){
+        System.out.println("What can I do for you?");
+
+        Scanner input =new Scanner(System.in);
+
+        String userInput = input.nextLine();
+
+        while(!userInput.equals("bye")){
+            System.out.println("   " + userInput);
+            userInput = input.nextLine();
+        }
+
+        System.out.println("Bye, Hope to see you again soon!");
+    }
+
+    static void level2ArrayList(){
+        System.out.println("What can I do for you?");
+
+        Scanner input = new Scanner(System.in);
+
+        String userInput;
+        ArrayList list = new ArrayList();
+
+        while(true){
+            userInput = input.nextLine();
+            if(userInput.equals("list")){
+                for(int i = 0; i < list.size(); i++){
+                    System.out.println((i+1) + "." + list.get(i));
+                }
+            }
+            else if(userInput.equals("bye")){
+                break;
+            }
+            else {
+                list.add(userInput);
+                System.out.println("added: " + userInput);
+            }
+        }
+
+        System.out.println("Bye, Hope to see you again soon!");
+    }
+
+    static void level2ArrayofString(){
+        System.out.println("What can I do for you?");
+
+        Scanner input = new Scanner(System.in);
+
+        String userInput;
+        String[] list = new String[100];
+        int k = 0;
+        while(true){
+            //if(k > 100) k = 100; //extra line for >100 input
+            userInput = input.nextLine();
+            if(userInput.equals("list")){
+                for(int i = 0; i < k; i++){
+                    System.out.println((i+1) + "." + list[i]);
+                }
+            }
+            else if(userInput.equals("bye")){
+                break;
+            }
+            else {
+                list[k++] = userInput;
+                System.out.println("added: " + userInput);
+            }
+        }
+
+        System.out.println("Bye, Hope to see you again soon!");
+    }
+
+    static void level3(){
+        System.out.println("What can I do for you?");
+
+        String[] list = new String[100];
+        //String[] list = {"read book", "return book", "buy bread"};
+        Scanner input = new Scanner(System.in);
+        String userInput; int k = 0;
+
+        while(true){
+            userInput = input.nextLine();
+
+            if(userInput.equals("list")){
+                //list items
+                for(int i = 0; i < k; i++){
+                    System.out.println((i+1) + ". " + list[i]);
+                }
+            }
+            else if(userInput.equals("bye")){
+                break;
+            }
+            else{
+                String[] option;
+                option = userInput.split(" ", 2);
+                if(option[0].equals("done")){
+                    int index = Integer.parseInt(option[1]);
+
+                    String[] temp;
+                    temp = list[index-1].split(" ", 2);
+
+                    temp[0] ="[✓]"; //convert option[1] to int
+                    list[index-1] = temp[0] + temp[1];
+                    System.out.println(list[index-1]);
+                    //list[index-1] = temp;
+                }
+                else {
+                    list[k++] =  "[✗] " + userInput;
+                    System.out.println("[✗] " + userInput);
+                }
+            }
+
+        }
+
+        System.out.println("Bye, Hope to see you again soon!");
+
+    }
+
+
 }
