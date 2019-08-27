3c6f4cc1394a2c618e7db70323a9b95f00a9f97c
hwbjerry <e0319255@u.nus.edu>
2019-08-28 01:53:17 +0800

    level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,11 +11,11 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        level4();
+        level5();
     }
 
+    static void level5() {
 
-    static void level4(){
         System.out.println("What can I do for you?");
 
         Scanner input = new Scanner(System.in);
@@ -23,50 +23,90 @@ public class Duke {
 
         ArrayList<Task> taskList = new ArrayList<Task>();
 
-        while(true){
-            userInput = input.nextLine();
+        while (true) {
+            try {
+                userInput = input.nextLine();
 
-            if(userInput.equals("list")){
-                System.out.println("Here are the tasks in your list:");
-                for(int i = 0; i < taskList.size(); i++)
-                    System.out.println((i+1) + ". " + taskList.get(i).getStatusIcon());
-            }
-            else if(userInput.equals("bye")){
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            }
-            else{
-                String[] split = userInput.split(" ", 2);//eg 1. deadline|return book /by Sunday
+                if (userInput.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < taskList.size(); i++) {
+                        System.out.println((i + 1) + ". " + taskList.get(i).getStatusIcon());
+                    }
+                } else if (userInput.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                } else {
+                    if (userInput.startsWith("todo")) {
+                        try {
+                            String taskOut = userInput.replaceFirst("todo", "");//todo| |read book spilt to three parts
+                            taskOut = taskOut.trim();
+                            if (taskOut.isEmpty()) {
+                                throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                            }
+                            taskList.add(new Todo(taskOut));
+                            System.out.println("Got it. I've added this task:");
+                            System.out.println("  " + taskList.get(taskList.size() - 1).getStatusIcon());
+                            System.out.println("Now you have " + taskList.size() + " tasks in the list.");
 
-                if(split[0].equals("todo")){
-                    taskList.add(new Todo(split[1]));
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  " + taskList.get(taskList.size()-1).getStatusIcon());
-                    System.out.println("Now you have " + taskList.size()+ " tasks in the list.");
-                }
-                else if(split[0].equals("deadline")){
-                    String[] taskDescription = split[1].split("/", 2); //2. return book |by Sunday
-                    String[] by = taskDescription[1].split(" ", 2);//3. by|sunday
-                    taskList.add(new Deadline(taskDescription[0], by[1]));
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  " + taskList.get(taskList.size()-1).getStatusIcon());
-                    System.out.println("Now you have " + taskList.size()+ " tasks in the list.");
-                }
-                else if(split[0].equals("event")){
-                    String[] taskDescription = split[1].split("/", 2); //eg read book
-                    String[] at = taskDescription[1].split(" ", 2);
-                    taskList.add(new Event(taskDescription[0], at[1]));
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  " + taskList.get(taskList.size()-1).getStatusIcon());
-                    System.out.println("Now you have " + taskList.size()+ " tasks in the list.");
-                }
-                else if(split[0].equals("done")){
-                    taskList.get(Integer.parseInt(split[1]) - 1).updateFlag();
-                    System.out.println("  " + taskList.get(Integer.parseInt(split[1]) - 1).getStatusIcon());
-                }
-            }
+                        } catch (DukeException e) {
+                            //catch exception
+                            System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+                            System.out.println("Please enter another task!");
+                            continue;
+                        }
+                    } else if (userInput.startsWith("deadline")) {
+                        try {
+                            String taskOut = userInput.replaceFirst("deadline", "");
+                            taskOut = taskOut.trim();
+                            if(taskOut.isEmpty()){
+                                throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                            }
+                            String[] by = taskOut.split(" /by ", 2);//3. by|sunday
+                            taskList.add(new Deadline(by[0], by[1]));
+                            System.out.println("Got it. I've added this task:");
+                            System.out.println("  " + taskList.get(taskList.size() - 1).getStatusIcon());
+                            System.out.println("Now you have " + taskList.size() + " tasks in the list.");
+                        } catch (DukeException exception) {
+                            System.out.println("Please enter another task! in the form: deadline 'description' /by 'due period'");
+                            continue;
+                        }
+                    } else if (userInput.startsWith("event")) {
+                        try {
+                            String taskOut = userInput.replaceFirst("event", "");
+                            taskOut = taskOut.trim();
+                            if(taskOut.isEmpty()){
+                                throw new DukeException("Please enter another task! In the form: event 'description' /at 'due period'");
+                            }
+                            String[] at = taskOut.split(" /at ", 2);
+                            taskList.add(new Event(at[0], at[1]));
+                            System.out.println("Got it. I've added this task:");
+                            System.out.println("  " + taskList.get(taskList.size() - 1).getStatusIcon());
+                            System.out.println("Now you have " + taskList.size() + " tasks in the list.");
 
+                        } catch (DukeException exception) {
+                            System.out.println("Please enter another task! In the form: event 'description' /at 'due period'");
+                            continue;
+                        }
+                    } else if (userInput.startsWith("done")) {
+                        String[] split = userInput.split(" ", 2);
+                        int index = Integer.parseInt(split[1]) - 1;
+                        if (index < 0 || index >= taskList.size()) {
+                            System.out.println("Invalid number!");
+                        } else {
+                            taskList.get(index).updateFlag();
+                            System.out.println("  " + taskList.get(index).getStatusIcon());
+                        }
+                    }
+                    else{
+                        throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    }
+                }
+            }catch(DukeException exception){
+                    System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                   // continue;
+            }
         }
+
     }
 
 
