9fe4720f64d465036bc21830ac933ca78bae1606
hwbjerry <e0319255@u.nus.edu>
2019-08-27 22:53:40 +0800

    level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,7 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
-
 public class Duke {
-
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -15,21 +11,32 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        level1();
+        level2ArrayList();
     }
 
 
-
-    static void level1(){
+    static void level2ArrayList(){
         System.out.println("What can I do for you?");
 
-        Scanner input =new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        String userInput = input.nextLine();
+        String userInput;
+        ArrayList list = new ArrayList();
 
-        while(!userInput.equals("bye")){
-            System.out.println("   " + userInput);
+        while(true){
             userInput = input.nextLine();
+            if(userInput.equals("list")){
+                for(int i = 0; i < list.size(); i++){
+                    System.out.println((i+1) + "." + list.get(i));
+                }
+            }
+            else if(userInput.equals("bye")){
+                break;
+            }
+            else {
+                list.add(userInput);
+                System.out.println("added: " + userInput);
+            }
         }
 
         System.out.println("Bye, Hope to see you again soon!");
